f4aaebbe20496f6fc328a5321926d373fb52b23b
Rishav Kumar <81378195+rishav-raj1@users.noreply.github.com>
2025-02-20 22:11:10 +0530

    tab navigator

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,12 +1,97 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+import SettingsScreen from '../screens/SettingsScreen';
+import CartScreen from '../screens/CartScreen';
+import HomeScreen from '../screens/HomeScreen';
+import GameDetailsScreen from '../screens/GameDetailsScreen';
+
+
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="GameDetails"
+        component={GameDetailsScreen}
+        options={({route}) => ({
+          title: route.params?.title,
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const TabNavigator = () => {
   return (
-    <View>
-      <Text>TabNavigator</Text>
-    </View>
-  )
-}
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {backgroundColor: '#4a73ff'},
+        tabBarInactiveTintColor: '#fff',
+        tabBarActiveTintColor: 'yellow',
+      }}>
+      <Tab.Screen
+        name="Home2"
+        component={HomeStack}
+        options={({route}) => ({
+          tabBarStyle: {
+            display: getTabBarVisibility(route),
+            backgroundColor: '#4a73ff',
+          },
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarBadge: 1,
+          tabBarBadgeStyle: {backgroundColor: 'yellow'},
+          tabBarIcon: ({color, size}) => (
+            <Feather name="shopping-bag" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-export default TabNavigator
+const getTabBarVisibility = route => {
+  // console.log(route);
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Feed';
+  // console.log(routeName);
+
+  if( routeName == 'GameDetails' ) {
+    return 'none';
+  }
+  return 'flex';
+};
+
+export default TabNavigator;
